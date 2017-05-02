7239d5c96c802902cf283194279188f1a7976f81
Adrian Pearman <adrianpearman12@gmail.com>
2017-05-02 19:57:01 -0400

    complete json request

--- election.js
+++ election.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
 
   // Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'json',
+  }).done(function(responseData){
+      for (var i = 0; i < responseData.candidates.length; i++) {
+          candidate = responseData.candidates[i];
 
+          var candidateInformation =  (' has recieved ' + candidate.votes + 'votes.');
+          var strongTag = $('<strong>').html(candidate.name);
+          var liTag = $('<li>').append(strongTag).append(candidateInformation);
+          $('#candidateContainer').append(liTag);
+        }
+  });
 });
+
+
+for (var i = 0; i < array.length; i++) {
+  array[i]
+}
